f352d68e5c3e4ab2ea1d1a313943b7122675cd6e
Somasz7096 <radkowski.tomasz@gmail.com>
2025-10-18 08:24:47 +0200

    Update config.py from Pythonista

--- config.py
+++ config.py
@@ -1,19 +1,7 @@
-#DISCORD_TOKEN = 'MTM1MDY5ODM1Nzg3MzU3ODA4NQ.GNad31.KR70vxnwPo27A7Q8zN4o_65ViDsCsMq28W57TY'
-DISCORD_TOKEN = 'MTM1MDY5ODM1Nzg3MzU3ODA4NQ.G01wvz.6Tms969JkRhELW-qdIb8e59VRnnBJ9UfbC2Mwo' #test-bot
-GUILD_ID = 1352409822162583572 # test-server
 
 DIBS_TIME = 20
 FARM_TIME = 20
 BLACKLIST_TIME = 120
 GLOBAL_BLACKLIST_TIME = 90
-
-
-POSTGRES = {
-    "user": "myuser",
-    "password": "haslo",
-    "database": "postgres",
-    "host": "141.144.246.135",
-    "port": "5432"
-}
 
 
